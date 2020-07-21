af37fe50172b260dfb1f1eb94652ee6f410e7e8b
Sourangshu Ghosh <sourangshug123@gmail.com>
2020-07-21 09:34:46 +0530

    Create s_len_trim.m

---
+++ s_len_trim.m
@@ -0,0 +1,28 @@
+function len = s_len_trim ( s )
+
+%*****************************************************************************80
+%
+%% S_LEN_TRIM returns the length of a character string to the last nonblank.
+%
+%  Licensing:
+%
+%    This code is distributed under the GNU LGPL license.
+%
+%
+%  Parameters:
+%
+%    Input, string S, the string to be measured.
+%
+%    Output, integer LEN, the length of the string up to the last nonblank.
+%
+  len = length ( s );
+
+  while ( 0 < len )
+    if ( s(len) ~= ' ' )
+      return
+    end
+    len = len - 1;
+  end
+
+  return
+end
